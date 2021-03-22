43931687fa9e30debc420ecae181c8331b390323
Vít Kilián <kilianv@spseplzen.cz>
2021-03-22 10:06:48 +0100

    npm run build a deploy

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll'
+import ErrorBoundry from '../Components/ErrorBoundry'
 import './App.css';
 
 class App extends Component {
@@ -35,7 +36,9 @@ class App extends Component {
             <h1 className='f1'>RoboZmrdi</h1>
             <SearchBox searchChange={this.onSearchChange} />
             <Scroll>
+            <ErrorBoundry>
             <CardList robots={filteredRobots} />
+            </ErrorBoundry>
             </Scroll>
             
         </div>     
